b48263f60ab5f639607fbaa3e54f3cb13dd128bc
Ymir <160019304+Ymir845@users.noreply.github.com>
2024-05-28 09:31:50 +1000

    Create RandomNum.java

---
+++ Tutorial/RandomNum.java
@@ -0,0 +1,11 @@
+// Must be the same name as file
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Synt");
+        // 'println' is on a different line
+        System.out.println("Number:");
+        int testVar = (int)(Math.random() * 51); // Random Number from 0 - 50
+
+        System.out.println(testVar); 
+    }
+}
